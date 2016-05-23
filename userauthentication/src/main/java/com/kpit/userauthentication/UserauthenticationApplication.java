d2f57d6dc86d778b77111339298daecc3ff51766
venkatr1 <venkatr1@kpit.com>
2016-05-23 14:33:37 +0530

    Initial code checkedin

---
+++ userauthentication/src/main/java/com/kpit/userauthentication/UserauthenticationApplication.java
@@ -0,0 +1,12 @@
+package com.kpit.userauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserauthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserauthenticationApplication.class, args);
+	}
+}
